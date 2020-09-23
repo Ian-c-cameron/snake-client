eaa6c9ef3ca9219d6ef3a6a8f170f7d5f832e4a7
Ian-c-cameron <ian.c.cam@gmail.com>
2020-09-23 20:04:32 +0000

    moved input functions to input.js module

--- play.js
+++ play.js
@@ -1,35 +1,19 @@
 let {connect} = require("./client");
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+let {setupInput} = require("./input");
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-setupInput();
 
 
 console.log('Connecting ...');
 let serv = connect();
+
+setupInput();
+
 
 serv.on('data', (data) => {
   console.log('Server says: ', data);
 });
 serv.on('connect', () => {
   serv.write("Name: BIG");
-  
 });
 
 setTimeout(() => {
